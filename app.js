81681b8f383c2c2f96c93a991c557a3e8b787139
JustinCLT <72402540+JustinCLT@users.noreply.github.com>
2021-02-24 22:57:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const socket = require('socket.io')
 
-var server = app.listen(5000, function () {
+var server = app.listen(process.env.PORT || 0.0.0.0, function () {
     console.log('Listening on port 5000')
 })
 
