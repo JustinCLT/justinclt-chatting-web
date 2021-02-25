cf48ec6eaf5987782f89356a754ca95022f53e68
JustinCLT <72402540+JustinCLT@users.noreply.github.com>
2021-02-25 20:11:58 +0700

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,4 @@
-
-const socket = io.connect(window.location.hostname)
-
+const socket = io.connect(`http://localhost:3000`)
 
 var pesan = document.getElementById('pesan');
 var nama = document.getElementById('name')
@@ -8,10 +6,19 @@ var output = document.getElementById('output')
 var button = document.getElementById('send')
 var feedback = document.getElementById('feedback')
 
+function t() {
+var dateObject = new Date()
+var t = dateObject.getHours() + ":" + menit(dateObject.getMinutes()) + ":" + (dateObject.getSeconds());
+return t
+}
+
+window.setInterval(t, 1000)
+
 button.addEventListener('click', () => {
     socket.emit('chat', {
         pesan: pesan.value,
-        name: nama.value
+        name: localStorage.getItem('username'),
+        waktu: t()
     })
 })
 
@@ -19,13 +26,25 @@ pesan.addEventListener('keypress', () => {
     socket.emit('is-typing',nama.value)
 })
 
+pesan.addEventListener('keyup', (e) => {
+    if (e.key === "Enter") {
+        socket.emit('chat', {
+            pesan: pesan.value,
+            name: localStorage.getItem('username'),
+            waktu: t()
+        })
+    }
+})
 
+if(!localStorage["username"]){
+    localStorage.setItem("username" , prompt("Masukkan namamu!"))
+}
   
 socket.on('chat', data => {
-    if (!data.name) return alert('Masukkan namamu!')
-    if (!data.pesan) return alert('Masukkan pesannya!')
+    if (!data.pesan.trim().length) return alert('Masukkan pesannya!')
     feedback.innerHTML = ""
-    output.innerHTML += `<p><strong>${data.name}</strong><br>${data.pesan}</p>`
+    output.innerHTML += `<p><strong>${data.name} (${data.waktu})</strong><br>${data.pesan}</p>`
+    pesan.value = ''
 })
 
 socket.on('connected', () => {
@@ -35,3 +54,11 @@ socket.on('connected', () => {
 socket.on('is-typing', data => {
     feedback.innerHTML = `<p><strong>${data}</strong> sedang mengetik...</p>`
 })
+
+
+function menit(x) {
+    if (x < 10) {
+    x = `0` + x
+}
+return x;
+}
